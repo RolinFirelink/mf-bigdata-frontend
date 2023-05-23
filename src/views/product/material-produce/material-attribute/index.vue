e49d3c14c7a3c71aff322a152dbe8fd8fcd468e1
“Rolin” <2259286198@qq.com>
2023-05-23 11:00:18 +0800

    🎨 产品模块数据表前端结构未完全提交部分提交(无Bug)

---
+++ src/views/product/material-produce/material-attribute/index.vue
@@ -0,0 +1,109 @@
+<!--
+ @description: 产品属性表
+ @author: cgli
+ @date: 2023-05-21
+ @version: V1.0.0
+-->
+<template>
+  <div>
+    <BasicTable @register="registerTable">
+      <template #toolbar>
+        <a-button type="primary" @click="handleCreate" v-if="hasPermission('sys:materialAttribute:insert')">新增产品属性表</a-button>
+      </template>
+      <template #bodyCell="{ column, record }">
+        <template v-if="column.key === 'action'">
+          <TableAction
+            :actions="[
+              {
+                icon: 'ant-design:edit-outlined',
+                onClick: handleEdit.bind(null, record),
+                auth: 'sys:materialAttribute:update',
+                tooltip: '修改',
+              },
+              {
+                icon: 'ant-design:delete-outlined',
+                color: 'error',
+                popConfirm: {
+                  title: '是否确认删除',
+                  placement: 'left',
+                  confirm: handleDelete.bind(null, record),
+                },
+                auth: 'sys:materialAttribute:delete',
+                tooltip: '删除',
+              },
+            ]"
+          />
+        </template>
+      </template>
+    </BasicTable>
+    <MaterialAttributeModal @register="registerModal" @success="handleSuccess" />
+  </div>
+</template>
+<script lang="ts">
+import { BasicTable, useTable, TableAction } from "/@/components/general/Table";
+import { deleteMaterialAttribute, getMaterialAttributeList } from "/@/api/material_attribute/MaterialAttribute";
+import { useModal } from "/@/components/general/Modal";
+import MaterialAttributeModal from "./MaterialAttributeModal.vue";
+import { columns, searchFormSchema } from "./materialAttribute.data";
+import { usePermission } from "/@/hooks/web/UsePermission";
+
+export default {
+  name: "MaterialAttributeManagement",
+  components: { BasicTable, MaterialAttributeModal, TableAction },
+  setup() {
+    const { hasPermission } = usePermission();
+    const [registerModal, { openModal }] = useModal();
+    const [registerTable, { reload }] = useTable({
+      title: "产品属性表列表",
+      api: getMaterialAttributeList,
+      columns,
+      formConfig: {
+        labelWidth: 100,
+        schemas: searchFormSchema
+      },
+      useSearchForm: true,
+      showTableSetting: true,
+      bordered: true,
+      showIndexColumn: false,
+      actionColumn: {
+        width: 80,
+        title: "操作",
+        dataIndex: "action"
+      }
+    });
+
+    function handleCreate() {
+      openModal(true, {
+        isUpdate: false
+      });
+    }
+
+    function handleEdit(record: Recordable) {
+      openModal(true, {
+        record,
+        isUpdate: true
+      });
+    }
+
+    function handleDelete(record: Recordable) {
+      deleteMaterialAttribute(record.id).then(() => {
+        handleSuccess();
+      });
+    }
+
+    function handleSuccess() {
+      reload();
+    }
+
+    return {
+      registerTable,
+      registerModal,
+      handleCreate,
+      handleEdit,
+      handleDelete,
+      handleSuccess,
+      hasPermission
+    };
+  }
+};
+</script>
